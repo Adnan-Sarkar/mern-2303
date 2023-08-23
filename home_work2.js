900cfb37cb4b725bf9c3ec9a1c886ec957b24e36
Aduvai1 <adnansarkar111@gmail.com>
2023-08-24 02:22:32 +0600

    Home work complete 2

---
+++ home_work2.js
@@ -0,0 +1,9 @@
+const start = 5;
+const end = 10;
+
+for (let i = start; i < end; i++) {
+  for (let j = 1; j <= 10; j++) {
+    console.log(`${j} x ${i} = ${j * i}`);
+  }
+  console.log("-----------------------------");
+}
